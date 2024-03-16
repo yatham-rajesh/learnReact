189ca3c8a9222a0f3a9a00bd590daac6365b73e4
Rajesh Yatham <rajeshnaniy@gmail.com>
2024-03-16 12:32:31 +0530

    add parcel

--- ReactDevelopment/Basics/App.js
+++ ReactDevelopment/Basics/App.js
@@ -1,4 +1,8 @@
-const heading = React.createElement("h1", {}, "Hello world from Rajesh");
+import React from 'react';
+import ReactDOM  from 'react-dom/client';
+
+
+const heading = React.createElement("h1", {}, "Hello world from Rajesh Yatham");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(heading);
--- ReactDevelopment/Basics/README.md
+++ ReactDevelopment/Basics/README.md
@@ -1,1 +1,1 @@
-# Learn React
+# Learn Reactn
